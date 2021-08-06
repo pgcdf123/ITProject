8fc69cf6f827214c9ab2e9aafcdf609e8bdddb6e
Weixuan Li <2512192l@student.gla.ac.uk>
2021-08-07 02:59:50 +0800

    profile

---
+++ static/js/bingo.js
@@ -0,0 +1,36 @@
+  $('.card').delegate('#zan','click',function (){
+    AddFavor(this)
+  })
+function AddFavor(self)
+  {
+
+    var fontSize=15;
+    var top=30;
+    var right=80;
+    var opacity=1;
+    var tag=document.createElement('span');
+    $(tag).text('+1');
+    $(tag).css('color','green');
+    $(tag).css('position','absolute');
+    $(tag).css('fontSize',fontSize+"px");
+    $(tag).css('top',top+"px");
+    $(tag).css('right',right+"px");
+    $(tag).css('opacity',opacity);
+    $(self).append(tag);
+    var obj=setInterval(function ()
+    {
+      fontSize=fontSize+5;
+      top=top-5;
+      right=right-5;
+      opacity=opacity-0.2;
+      $(tag).css('fontSize',fontSize+"px");
+      $(tag).css('top',top+"px");
+      $(tag).css('right',right+"px");
+      $(tag).css('opacity',opacity);
+      if(opacity<0)
+      {
+        clearInterval(obj);
+        $(tag).remove();
+      }
+    },100)
+  }
